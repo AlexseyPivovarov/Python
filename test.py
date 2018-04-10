70789323684c3ff97ae0dee05b274f8c03158dcd
AlexseyPivovarov <a.peevovarov@gmail.com>
2018-04-10 05:19:41 +0300

    lesson2 hard 1.1

--- test.py
+++ test.py
@@ -22,43 +22,69 @@ for index in range(4):
             break
     if stop == 1:
         break
-# logical block
+# logical and output block
 if stop == 0:
-    # preparation for the loop
-    x = [number[0], number[2]]
-    y = [number[1], number[3]]
-    print("Вы ввели А({}, {}); B({}, {})".format(x[0], y[0], x[1], y[1]))
-    point = []
-    # positioning of points and set it to pointA and pointB
-    for index in [0, 1]:
-        if x[index] > 0:
-            if y[index] > 0:
-                point.insert(index, 1)
-            elif y[index] < 0:
-                point.insert(index, 4)
-            else:
-                point.insert(index, "+X")
-        elif x[index] < 0:
-            if y[index] > 0:
-                point.insert(index, 2)
-            elif y[index] < 0:
-                point.insert(index, 3)
-            else:
-                point.insert(index, "-X")
-        else:
-            if y[index] > 0:
-                point.insert(index, "+Y")
-            elif y[index] < 0:
-                point.insert(index, "-Y")
-            else:
-                point.insert(index, 0.0)
-    pointA = point[0]
-    pointB = point[1]
     x1 = number[0]
     y1 = number[1]
     x2 = number[2]
     y2 = number[3]
-    print("pointA = {}, pointB = {}".format(pointA, pointB))
-    # comparison position of points
+    # comparison position of line
+    text2 = "Отрезок"
+    if x1 == x2 and y1 == y2:
+        text2 = "Нулевой отрезок"
+    if x1 == 0 and y1 == 0 and x2 == 0 and y2 == 0:
+        print("{} находится в центре координат".format(text2))
+    elif y1 == 0 and y2 == 0:
+        print("{} находится на оси Х".format(text2))
+    elif x1 == 0 and x2 == 0:
+        print("{} находится на оси Y".format(text2))
+    else:
+        # some magic
+        if x1 == 0:
+            signX1 = int(x2 / abs(x2))
+        else:
+            signX1 = int(x1 / abs(x1))
+        if y1 == 0:
+            signY1 = int(y2 / abs(y2))
+        else:
+            signY1 = int(y1 / abs(y1))
+        if x2 == 0:
+            signX2 = int(x1 / abs(x1))
+        else:
+            signX2 = int(x2 / abs(x2))
+        if y2 == 0:
+            signY2 = int(y1 / abs(y1))
+        else:
+            signY2 = int(y2 / abs(y2))
+        # print("signA({}, {}); signB({}, {})".format(signX1, signY1, signX2, signY2))
+        text = "|что-то пошло нетак|"
+        if signX1 == signX2 and signY1 == signY2:
+            if signX1 == 1 and signY1 == 1:
+                text = "в первой"
+            elif signX1 == -1 and signY1 == 1:
+                text = "во второй"
+            elif signX1 == -1 and signY1 == -1:
+                text = "в третей"
+            else:
+                text = "в четвертой"
+            print("{} находится {} четверти".format(text2, text))
+        elif signX1 == signX2 and signY1 != signY2:
+            if signX1 == 1:
+                text = "первой и четвертой"
+            else:
+                text = "второй и третей"
+            print("Отрезок находится в смежных {} четвертях".format(text))
+        elif signX1 != signX2 and signY1 == signY2:
+            if signY1 == 1:
+                text = "первой и второй"
+            else:
+                text = "третей и четвертой"
+            print("Отрезок находится в смежных {} четвертях".format(text))
+        else:
+            if signX1 == signY1:
+                text = "первой и третей"
+            else:
+                text = "второй и четвертой"
+            print("Отрезок находится в диагональных {} четвертях".format(text))
 print("Готово!")
 
