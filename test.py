e0db031fda56be98cefab53f88424767f8052d99
AlexseyPivovarov <a.peevovarov@gmail.com>
2018-04-10 16:40:30 +0300

    lesson2 hard 1.2

--- test.py
+++ test.py
@@ -35,9 +35,21 @@ if stop == 0:
     if x1 == 0 and y1 == 0 and x2 == 0 and y2 == 0:
         print("{} находится в центре координат".format(text2))
     elif y1 == 0 and y2 == 0:
-        print("{} находится на оси Х".format(text2))
+        if x1 >= 0 and x2 >= 0:
+            text4 = "OX"
+        elif x1 <= 0 and x2 <= 0:
+            text4 = "-OX"
+        else:
+            text4 = "X и проходит через центр"
+        print("{} находится на оси {}".format(text2, text4))
     elif x1 == 0 and x2 == 0:
-        print("{} находится на оси Y".format(text2))
+        if y1 >= 0 and y2 >= 0:
+            text4 = "OY"
+        elif y1 <= 0 and y2 <= 0:
+            text4 = "-OY"
+        else:
+            text4 = "Y и проходит через центр"
+        print("{} находится на оси {}".format(text2, text4))
     else:
         # some magic
         if x1 == 0:
@@ -57,7 +69,7 @@ if stop == 0:
         else:
             signY2 = int(y2 / abs(y2))
         # print("signA({}, {}); signB({}, {})".format(signX1, signY1, signX2, signY2))
-        text = "|что-то пошло нетак|"
+        # text = "|что-то пошло нетак|"
         if signX1 == signX2 and signY1 == signY2:
             if signX1 == 1 and signY1 == 1:
                 text = "в первой"
@@ -81,10 +93,28 @@ if stop == 0:
                 text = "третей и четвертой"
             print("Отрезок находится в смежных {} четвертях".format(text))
         else:
+            # если отрезок в диагональных четвертях
+            if y1 < 0:
+                x1, x2 = x2, x1
+                y1, y2 = y2, y1
+            tgAB = (abs(x1) + abs(x2)) / (abs(y1) + abs(y2))
+            tgAO = abs(x1) / abs(y1)
             if signX1 == signY1:
                 text = "первой и третей"
+                if tgAB > tgAO:
+                    text3 = "смежной второй четверти"
+                elif tgAB < tgAO:
+                    text3 = "смежной четвертой четверти"
+                else:
+                    text3 = "проходит через центр"
             else:
                 text = "второй и четвертой"
-            print("Отрезок находится в диагональных {} четвертях".format(text))
+                if tgAB > tgAO:
+                    text3 = "смежной первой четверти"
+                elif tgAB < tgAO:
+                    text3 = "смежной третей четверти"
+                else:
+                    text3 = "проходит через центр"
+            print("Отрезок находится в диагональных {} четвертях и {}".format(text, text3))
 print("Готово!")
 
