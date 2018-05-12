bb54923ada495811961a2184e0037c50e3953b47
AlexseyPivovarov <a.peevovarov@gmail.com>
2018-05-12 18:06:45 +0300

    16.04

---
+++ libraries/decorator.py
@@ -0,0 +1,9 @@
+def time(func):
+    def wrapper(*args):
+        from time import time
+        start = time()
+        a = func(*args)
+        end = time()
+        print("{} time {}".format(func.__name__, round(end - start, 6)))
+        return a
+    return wrapper
